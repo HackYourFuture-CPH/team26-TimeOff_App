f398dac9b8858d0721edcae4d9a0bfde86d664eb
Nico <akebra77@gmail.com>
2024-03-25 09:16:39 +0100

    Refactoring of Main Components

--- src/backend/API/teams.js
+++ src/backend/API/teams.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.get('/api/teams', async (req, res) => {
   try {
-    const teams = await db.select().from('team'); 
+    const teams = await db.select().from('teams'); 
     res.json(teams);
   } catch (error) {
     console.error('Error fetching teams:', error);
@@ -24,9 +24,9 @@ app.get('/api/teams', async (req, res) => {
 
 app.post('/api/teams', async (req, res) => {
   try {
-    const { title, code } = req.body; // Aquí se espera que el campo se llame 'title'
+    const { team_name, team_code } = req.body; 
     const creationDate = new Date();
-    await db('team').insert({ title, code, created_date: creationDate }); // Insertar 'title' en lugar de 'name'
+    await db('teams').insert({ team_name, team_code, created_date: creationDate }); 
     res.status(201).json({ message: 'Team created successfully' });
   } catch (error) {
     console.error('Error creating team:', error);
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,15 +4,15 @@ import CreateTeam from '../component/CreateTeam';
 import '../index.css';
 
 const Main = () => {
-  const [teamCode, setTeamCode] = useState('');
   const [teamsDatabase, setTeamsDatabase] = useState([]);
 
   useEffect(() => {
     fetchTeams();
   }, []);
+
   const fetchTeams = async () => {
     try {
-      const response = await fetch('http://localhost:4050/api/teams'); // Adjust URL as needed
+      const response = await fetch('http://localhost:4050/api/teams');
       if (!response.ok) {
         throw new Error('Failed to fetch teams');
       }
@@ -26,12 +26,8 @@ const Main = () => {
   return (
     <div className="top-Header">
       <h2>Time Off</h2>
-      <SubmitTeam teamCode={teamCode} teamsDatabase={teamsDatabase} />
-      <CreateTeam
-        teamsDatabase={teamsDatabase}
-        setTeamsDatabase={setTeamsDatabase}
-        setTeamCode={setTeamCode}
-      />
+      <SubmitTeam teamsDatabase={teamsDatabase} />
+      <CreateTeam setTeamsDatabase={setTeamsDatabase} />
     </div>
   );
 };
