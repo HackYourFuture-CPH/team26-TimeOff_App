a16285ea41788e33c91daf3a2bde6f2704b5ea4b
shazkhan2 <seewardak@hotmail.com>
2024-04-08 12:16:45 +0200

    Delete team component removed

---
+++ src/component/DeleteMember.jsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import React from 'react'
+import {apiPath} from "../api"
+const DeleteMember = ({ memberId }) => {
+    const navigate = useNavigate();
+
+    const handleDeleteMember = async () => {
+        try {
+    
+          const response = await fetch(apiPath(`/members/${memberId}`), {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          });
+    
+          if (!response.ok) {
+            throw new Error('Failed to delete member');
+        } else {
+            navigate(`/team/${randomCode}`);
+        }
+    } catch (error) {
+        console.error(error); 
+    }
+};
+  return (
+    <div>
+        <button onClick={handleDeleteMember(memberId)}>Delete</button>                
+         
+        
+    </div>
+)
+};
+export default DeleteMember;
