ab4adbaaf8b211f3a51e1bc067faf5e983c944f2
Srushti Salke <srushtisalke380@gmail.com>
2024-04-10 15:38:10 +0200

    Added light and dark mode. Update Navbar with search field and other styling as well

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './pages/Navbar';
+import './index.css'
 import Home from './pages/Home';
 import Team from './pages/Team';
 import MemberPage from "./pages/Member";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const current_theme = localStorage.getItem('current_theme');  // This will store the default theme of web browser
+  const [theme, setTheme] = useState(current_theme ? current_theme : 'light'); // Created constant for dark mode
+
+  useEffect (()=>{
+    localStorage.setItem('current_theme', theme);
+  },[theme])
+
+
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <Router>
-        <Navbar />     
+        <Navbar theme={theme} setTheme={setTheme} />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/team/:code" element={<Team />} />
